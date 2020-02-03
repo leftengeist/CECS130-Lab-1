40b938cedc17cbb40c9505d73b9f199e4da6925d
Levi Walton <22774739+leftengeist@users.noreply.github.com>
2020-02-03 14:53:45 -0500

    Added Files

---
+++ CECS130Lab1Walton_Levi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+This Program is for visualizing the shape of Dr. Yampalski
+Walton Levi, Section 1
+*/
+main()
+{
+	printf("             _____\n");
+	printf("           /######\\\n");
+	printf("           | o # ##|\n");
+	printf("          /_      #|\n");
+	printf("           \\__   #/\n");
+	printf("            |__  |\n");
+	printf("              |  |\n");
+	printf("          Dr. Yampalsky");
+	return 0;
+}
